c0b263f3c6e10414f1ac4ee50dc3d1c8f979d54e
alefwhite <alefwhite@hotmail.com>
2024-05-26 21:55:06 -0300

    chore: adicionado fastify cookies

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,23 @@ import { env } from "@/env"
 import { usersRoutes } from "@/http/controllers/users/route"
 import useCaseErrors from "@/usecases/errors"
 import fastifyJwt from "@fastify/jwt"
+import fastifyCookie from "@fastify/cookie"
+import { appRoutes } from "@/http/routes"
 
 export const app = fastify()
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
   sign: {
     expiresIn: "10m",
   },
 })
+app.register(fastifyCookie)
 app.register(usersRoutes)
+app.register(appRoutes)
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply.status(400).send({
